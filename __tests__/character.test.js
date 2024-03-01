a25de393d67adaabf8d9ee26c87df8b5bdfbadd4
Brian Scherner <brianscherner@gmail.com>
2024-03-01 11:19:01 -0800

    Write passing test to create different attributes for different characters.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,3 +1,4 @@
+import { experiments } from "webpack";
 import { createCharacter, assignAttributes } from "../src/js/character.js";
 
 describe('RPG game character creation tests', () => {
@@ -11,10 +12,32 @@ describe('RPG game character creation tests', () => {
     expect(createdCharacter3.type).toEqual("scout");
   });
 
-  test('Should be able to assign values for base attributes for a character.', () => {
+  test('Should be able to assign a value for a base attribute for a character.', () => {
     const createdCharacter = createCharacter("mechanic");
     const setAttributes = assignAttributes("intelligence")(8);
     const result = setAttributes(createdCharacter);
     expect(result).toEqual({ "type": "mechanic", "intelligence": 8 });
+  });
+
+  test('Should be able to add different attributes to different characters.', () => {
+    const mechanic = createCharacter('mechanic');
+    const enforcer = createCharacter('enforcer');
+    const sergeant = createCharacter('sergeant');
+    const president = createCharacter('president');
+
+    const setAttributesMechanic = assignAttributes('endurance')(7);
+    const setAttributesEnforcer = assignAttributes('melee')(9);
+    const setAttributesSergeant = assignAttributes('strength')(8);
+    const setAttributesPresident = assignAttributes('charisma')(9);
+
+    const mechanicResult = setAttributesMechanic(mechanic);
+    const enforcerResult = setAttributesEnforcer(enforcer);
+    const sergeantResult = setAttributesSergeant(sergeant);
+    const presidentResult = setAttributesPresident(president);
+
+    expect(mechanicResult).toEqual({ "type": "mechanic", "endurance": 7 });
+    expect(enforcerResult).toEqual({ "type": "enforcer", "melee": 9 });
+    expect(sergeantResult).toEqual({ "type": "sergeant","strength": 8 });
+    expect(presidentResult).toEqual({ "type": "president","charisma": 9 });
   });
 });
